23a78c042483973af4bbb6fcb3d1e5e9655c30a0
Danelson Rosa <danelson.rosa@gmail.com>
2017-07-29 02:31:00 -0400

    WIP: profile show page.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 		viewShowPage(this);
 	});
 
+	// $(".view-profile").hover(function() {
+	// 	$(this).addClass("animated pulse");
+	// 	$(this).removeClass("animated pulse")
+	// });
+
 	function viewShowPage(card) {
 		var $card = $(card);
 		var url = "/" + $card.data("profile");
