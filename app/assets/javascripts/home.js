c792020a0a15bc7f9d0cc1bd36eb98f97efb94a7
Danelson Rosa <danelson.rosa@gmail.com>
2017-08-04 22:48:11 -0400

    Makes frontend updates.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,11 +3,6 @@ $(document).ready(function() {
 		viewShowPage(this);
 	});
 
-	// $(".view-profile").hover(function() {
-	// 	$(this).addClass("animated pulse");
-	// 	$(this).removeClass("animated pulse")
-	// });
-
 	function viewShowPage(card) {
 		var $card = $(card);
 		var url = "/" + $card.data("profile");
